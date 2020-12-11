0d7558d0e3fa9a5f78e23136716d9022a14ff90d
Tamas Nagy <nredline@gmail.com>
2020-12-11 22:13:14 +0100

    Implement day11 part2 solution

--- day11.ts
+++ day11.ts
@@ -4,7 +4,7 @@ const inputString = await fetchInput(11);
 
 let inputMatrix = inputString.split("\n").map((x) => [...x]);
 
-const countOccupied = (row: number, column: number) => {
+const countOccupied = (row: number, column: number, lookFarAway? : boolean) => {
   let numNeighbors = 0;
   for (let x = row - 1; x <= row + 1; x++) {
     for (let y = column - 1; y <= column + 1; y++) {
@@ -16,7 +16,7 @@ const countOccupied = (row: number, column: number) => {
         continue;
       }
 
-      if (inputMatrix[x][y].startsWith("#")) {
+        if(lookInDirection(row, column, { rowStep:x - row, columnStep:y - column }, lookFarAway)){
         numNeighbors++;
       }
     }
@@ -34,8 +34,11 @@ const lookInDirection = (row: number, column: number, direction: {rowStep:number
 
   if(inputMatrix[nextRow][nextColumn].startsWith("#"))
     return true
+  
+  if(inputMatrix[nextRow][nextColumn].startsWith("L"))
+    return false
 
-  return lookFarAway ? lookInDirection(nextRow, nextColumn, direction) : false
+  return lookFarAway ? lookInDirection(nextRow, nextColumn, direction, lookFarAway) : false
 }
 
 //# -> occupied seat
@@ -43,40 +46,38 @@ const lookInDirection = (row: number, column: number, direction: {rowStep:number
 //. -> no seat
 //#L -> occupied seat which will be empty
 //L# -> empty seat which will be occupied
-let changesInTurn = 0;
-let numTurns = 0;
-do {
-  changesInTurn = 0;
+const runSimulation = (seatOccupationTolerance: number, lookFarAway: boolean) => {
+  let changesInTurn = 0;
+  let numTurns = 0;
+  do {
+    changesInTurn = 0;
+    for (let row = 0; row < inputMatrix.length; row++) {
+      for (let column = 0; column < inputMatrix[row].length; column++) {
+        if (inputMatrix[row][column].startsWith("L")) {
+          const occupiedNeighbors = countOccupied(row, column, lookFarAway);
+          if (occupiedNeighbors === 0) {
+            inputMatrix[row][column] += "#";
+            changesInTurn++;
+          }
+        } else if (inputMatrix[row][column].startsWith("#")) {
+          
+          const occupiedNeighbors = countOccupied(row, column, lookFarAway);
 
-  for (let row = 0; row < inputMatrix.length; row++) {
-    for (let column = 0; column < inputMatrix[row].length; column++) {
-      if (inputMatrix[row][column].startsWith("L")) {
-        const occupiedNeighbors = countOccupied(row, column);
-        if (occupiedNeighbors === 0) {
-          inputMatrix[row][column] += "#";
-          changesInTurn++;
-        }
-      } else if (inputMatrix[row][column].startsWith("#")) {
-        
-        const occupiedNeighbors = countOccupied(row, column);
-
-        // if(row === 0 && column === 2){
-        //   console.log()
-        // }
-        if (occupiedNeighbors >= 4) {
-          inputMatrix[row][column] += "L";
-          changesInTurn++;
+          if (occupiedNeighbors >= seatOccupationTolerance) {
+            inputMatrix[row][column] += "L";
+            changesInTurn++;
+          }
         }
       }
     }
-  }
 
-  // console.table(inputMatrix)
+    inputMatrix = inputMatrix.map((row) => [...row.map((char) => char[char.length - 1])]);
+    numTurns++;
+  } while (changesInTurn > 0);
 
-  inputMatrix = inputMatrix.map((row) => [...row.map((char) => char[char.length - 1])]);
-  numTurns++;
-} while (changesInTurn > 0);
+  console.log(inputMatrix.reduce((a, row) => a + row.reduce((aa, char) => aa + (char === "#" ? 1 : 0), 0), 0))
+}
 
-console.log(numTurns)
 // console.table(inputMatrix)
-console.log(inputMatrix.reduce((a, row) => a + row.reduce((aa, char) => aa + (char === "#" ? 1 : 0), 0), 0))
+runSimulation(4, false)
+runSimulation(5, true)
