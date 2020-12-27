d3e3318af435d2a2bdb48d5ce54a6dfaf6736a3f
Tamas Nagy <nredline@gmail.com>
2020-12-27 17:10:16 +0100

    Implement part2 of day22

--- day22.py
+++ day22.py
@@ -8,34 +8,59 @@ player2 = list(map(lambda x: int(x), player2.splitlines()[1:]))
 player1.reverse()
 player2.reverse()
 
-turn = 1
+def calculatedeckhash(player1, player2):
+  return str(player1) + str(player2)
 
-while len(player1) > 0 and len(player2) > 0:
-  print("round", turn)
-  print("p1:", player1)
-  print("p2:", player2)
-  
-  card1 = player1.pop()
-  card2 = player2.pop()
+def recursiveCombat(player1, player2):
+  turn = 1
+  existingsetups = set()
 
-  print("drawn cards:", card1, card2)
-  print()
-  if card1 > card2:
-    player1.insert(0, card1)
-    player1.insert(0, card2)
-  else:
-    player2.insert(0, card2)
-    player2.insert(0, card1)
+  while len(player1) > 0 and len(player2) > 0:
+    deckhash = calculatedeckhash(player1, player2)
 
-  turn = turn + 1
+    if deckhash in existingsetups:
+      print("Infinite game prevention, player1 wins", deckhash)
+      return (1, player1)
 
-winner = player2 if len(player2) > 0 else player1
+    existingsetups.add(deckhash)
+
+    # print("round", turn)
+    # print("p1:", player1)
+    # print("p2:", player2)
+    
+    card1 = player1.pop()
+    card2 = player2.pop()
+
+    if len(player1) >= card1 and len(player2) >= card2:
+      (winnerId, winnerDeck) = recursiveCombat(player1[-card1:], player2[-card2:])
+
+      if winnerId == 1:
+        player1.insert(0, card1)
+        player1.insert(0, card2)
+      else:
+        player2.insert(0, card2)
+        player2.insert(0, card1)
+
+    else:
+      if card1 > card2:
+        player1.insert(0, card1)
+        player1.insert(0, card2)
+      else:
+        player2.insert(0, card2)
+        player2.insert(0, card1)
+
+  winnerId = 2 if len(player2) > 0 else 1
+  winnerDeck = player2 if len(player2) > 0 else player1
+  return (winnerId, winnerDeck)
 
 
+(winnerId, winnerDeck) = recursiveCombat(player1, player2)
 result_score = 0
-multiplier = len(winner)
-for value in reversed(winner):
+multiplier = len(winnerDeck)
+for value in reversed(winnerDeck):
   result_score = result_score + (multiplier * value)
   multiplier = multiplier - 1
 
+print(winnerId)
+print(winnerDeck)
 print(result_score)
