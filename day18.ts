47a8eb22d88b6b34e2cb84bd00923c826e21960d
Tamas Nagy <nredline@gmail.com>
2020-12-27 17:11:37 +0100

    Add part2 of day18

--- day18.ts
+++ day18.ts
@@ -1,6 +1,6 @@
 import { fetchInput } from "./common_ts/inputAccess.ts";
 
-const lines = (await fetchInput(18)).split("\n");
+const lines = (await fetchInput(18)).trimEnd().split("\n");
 
 const parsedLines = lines.map(line =>line.split(" ").join("").split(""))
 
@@ -9,7 +9,7 @@ const executeOperation = (lhs: number, operand: string, rhs: number) => {
     return lhs * rhs
   else if (operand === "+")
     return lhs + rhs
-  else throw "unknown operand"
+  else throw "unknown operand" + operand
 }
 
 const solveSubEquation = (equationPortion: Array<string>) => {
@@ -26,7 +26,32 @@ const solveSubEquation = (equationPortion: Array<string>) => {
   return equationPortion[0]
 }
 
+const solvePrecedentedSubEquation = (equationPortion: Array<string>) => {
+  let index = 0
+  // console.log("precedented:", equationPortion)
+  while(index + 2 < equationPortion.length && equationPortion.length > 1){
+    const lhs = Number(equationPortion[index])
+    const operand = equationPortion[index + 1]
+    const rhs = Number(equationPortion[index + 2])
+    
+    // console.log(index, equationPortion, `${lhs} ${operand} ${rhs}`)
+
+    if(operand === "*")
+      index = index + 2
+    else {
+      const result = executeOperation(lhs, operand, rhs)
+      equationPortion.splice(index, 3, result.toString())
+    }
+  }
+
+  if(equationPortion.length === 1)
+    return equationPortion[0]
+  
+  return solveSubEquation(equationPortion)
+}
+
 const solveEquation = (equation: Array<string>) => {
+  equation = [...equation]
   const parentheses = []
 
   // console.log(equation)
@@ -41,20 +66,20 @@ const solveEquation = (equation: Array<string>) => {
         throw Error("Leave me alone i know what i am doing.")
   
       const subOperation = equation.slice(openingParentheses + 1, i)
-      const result = solveSubEquation(subOperation)
+      const result = solvePrecedentedSubEquation(subOperation)
       equation.splice(openingParentheses, i - openingParentheses + 1, result)
       i = openingParentheses
     }
   }
   
   if(equation.length > 1){
-    return solveSubEquation(equation)
+    return solvePrecedentedSubEquation(equation)
   }
 
   return equation[0]
 }
 
-const solvedEquations = parsedLines.map(x => solveEquation(x))
-console.log(solvedEquations.map(x => +x).filter(x => !x))
+const solvedEquations = [...parsedLines].map(x => solveEquation(x))
+console.log(solvedEquations.map(x => +x))
 const sum = solvedEquations.reduce((a, e) => a + Number(e), 0)
 console.log(sum)
